8d4fbc4879bf357317dc9cb69ec7c984bc4958aa
yswai1 <fvinet9@gmail.com>
2024-04-24 10:55:03 +0200

    L'utilisateur peut envoyer des apostrophes dans ses mails

--- functions/database.php
+++ functions/database.php
@@ -28,20 +28,20 @@ function run_query(string $query) {
  *
  * return bolean
  */
-function insert(string $table, array $datas) {
+function insert($connection, string $table, array $datas) {
     $dataColumn = null;
     $dataValues = null;
     foreach($datas as $column => $values) {
         $dataColumn .= $column . ",";
-        $dataValues .= "'" . $values . "',";
+        $dataValues .= "'" . mysqli_real_escape_string($connection, $values) . "',";
     }
 
-    $dataColumn = rtrim($dataColumn,',');
-    $dataValues = rtrim($dataValues,',');
+    $dataColumn = rtrim($dataColumn, ",");
+    $dataValues = rtrim($dataValues, ",");
 
     $query = "INSERT INTO {$table} ({$dataColumn}) VALUES({$dataValues})";
 
-    return run_query($query);
+    return run_query($query, $connection);
 }
 
 /**
