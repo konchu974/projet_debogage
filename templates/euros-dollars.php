cd0605a31b06203d30907b692c9bf57c5c651adc
Frederic Vinet <fvinet9@gmail.com>
2024-05-05 18:21:28 +0200

    Update

--- templates/euros-dollars.php
+++ templates/euros-dollars.php
@@ -1,31 +1,41 @@
 <?php
-   
-    template('header', array(
-        'title' => 'Boite à outils • Convertisseur',
-    ));
+
+template('header', array(
+    'title' => 'Boite à outils • Convertisseur',
+));
 ?>
 
-<!-- Section du convertisseur de devises (Euros) -->
+<!-- ======= Currency Converter Section ======= -->
 <section id="currency-converter" class="currency-converter mt-2 ms-5 me-5">
     <div class="container">
         <div class="section-title">
-          
             <h2>Convertisseur Euros</h2>
         </div>
-        <!-- Formulaire pour convertir les euros en une autre devise -->
+        <!-- Formulaire de conversion de devises -->
         <fieldset class="col-12 mt-2 p-3">
             <legend>Convertisseur de devise</legend>
             <form action="" method="post" name="euros-dollars">
                 <div class="row">
-                    <!-- Champ pour le montant en euros -->
+                    <!-- Champ pour entrer le montant à convertir -->
                     <div class="col">
                         <label for="montant" class="form-label visually-hidden">Montant à convertir :</label>
                         <div class="input-group">
                             <input type="number" id="montant" name="montant" class="form-control" required>
+                            <!-- Sélection de la devise source -->
                             <div class="input-group-append">
-                                <!-- Sélecteur pour choisir la devise source -->
-                                <select class="form-select" aria-label="Devise source" id="devisesource" name="devisesource" required>
+                                <select class="form-select" aria-label="Default select example" id="devisesource" name="devisesource" required>
                                     <?php
+                                    // URL de l'API pour obtenir les taux de change
+                                    $url = 'https://open.er-api.com/v6/latest';
+
+                                    // Récupération des données JSON de l'API
+                                    $data = file_get_contents($url);
+                                    $data = json_decode($data, true);
+
+                                    // Récupération des devises disponibles
+                                    $devises = array_keys($data['rates']);
+
+                                    // Génération des options de devise
                                     foreach ($devises as $devise) {
                                         echo "<option value=\"{$devise}\">{$devise}</option>";
                                     }
@@ -35,7 +45,7 @@
                         </div>
                     </div>
 
-                    
+                    <!-- Indicateur de conversion -->
                     <div class="col-auto align-self-center">
                         <span class="ver">vaut actuellement:</span>
                     </div>
@@ -44,12 +54,12 @@
                     <div class="col">
                         <label for="devisecible" class="form-label visually-hidden">Résultat</label>
                         <div class="input-group">
-                            <input id="devisecible" name="devisecible" type="text" class="form-control" disabled>
+                            <input id="devisecible" name="devisecible" type="text" class="form-control " disabled>
+                            <!-- Sélection de la devise cible -->
                             <div class="input-group-append">
-                                <!-- Sélecteur pour choisir la devise cible -->
-                                <select class="form-select" aria-label="Devise cible" id="devisecible" name="devisecible" required>
+                                <select class="form-select" aria-label="Default select example" id="devisecible" name="devisecible" required>
                                     <?php
-                                    // Génère les options de devise à partir des données de l'API
+                                    // Génération des options de devise (mêmes devises que pour la source)
                                     foreach ($devises as $devise) {
                                         echo "<option value=\"{$devise}\">{$devise}</option>";
                                     }
@@ -59,6 +69,7 @@
                         </div>
                     </div>
 
+                    <!-- Bouton pour soumettre le formulaire -->
                     <div class="col-auto">
                         <button name="submit" type="submit" class="btn btn-primary">Calculer</button>
                     </div>
@@ -66,47 +77,49 @@
             </form>
         </fieldset>
     </div>
-</section>
+</section><!-- End Currency Converter Section -->
 
-<!-- Section du convertisseur de liquides -->
-<section id="liquid-converter" class="liquid-converter ms-5 me-5">
+<!-- ======= Liquid Converter Section ======= -->
+<section id="liquid-converter" class="currency-converter ms-5 me-5">
     <div class="container">
         <div class="section-title">
-           
             <h2>Convertisseur de Liquide</h2>
         </div>
-        <!-- Formulaire pour convertir les millilitres en litres -->
+        <!-- Formulaire de conversion de liquide -->
         <fieldset class="col-12 mt-2 p-3">
             <legend>Convertisseur de Millilitres en Litres</legend>
             <form action="" method="post" name="mL-L">
                 <div class="row">
-                    <!-- Champ pour le montant en millilitres -->
+                    <!-- Champ pour entrer le montant à convertir -->
                     <div class="col">
                         <label for="mil" class="form-label visually-hidden">Montant à convertir :</label>
                         <div class="input-group">
                             <input id="mil" name="mil" type="number" class="form-control" required>
+                            <!-- Indicateur de l'unité de mesure -->
                             <div class="input-group-append">
                                 <div class="input-group-text">mL</div>
                             </div>
                         </div>
                     </div>
 
-                    <!-- Texte indiquant la devise cible -->
+                    <!-- Indicateur de conversion -->
                     <div class="col-auto align-self-center">
                         <span class="ver">vaut :</span>
                     </div>
 
-                    <!-- Champ pour afficher le résultat de la conversion en litres -->
+                    <!-- Champ pour afficher le résultat de la conversion -->
                     <div class="col">
                         <label for="litre" class="form-label visually-hidden">Résultat</label>
                         <div class="input-group">
                             <input id="litre" name="litre" type="text" class="form-control" disabled>
+                            <!-- Indicateur de l'unité de mesure -->
                             <div class="input-group-append">
                                 <div class="input-group-text">L</div>
                             </div>
                         </div>
                     </div>
 
+                    <!-- Bouton pour soumettre le formulaire -->
                     <div class="col-auto">
                         <button name="submit" type="submit" class="btn btn-primary">Calculer</button>
                     </div>
@@ -114,38 +127,41 @@
             </form>
         </fieldset>
 
-        <!-- Formulaire pour convertir les litres en millilitres -->
+        <!-- Formulaire de conversion inverse de liquide -->
         <fieldset class="col-12 mt-2 p-3">
             <legend>Convertisseur de Litres en Millilitres</legend>
-            <form action="" method="post" name="L-mL">
+            <form action="" method="post" name="mL-L">
                 <div class="row">
-                    <!-- Champ pour le montant en litres -->
+                    <!-- Champ pour entrer le montant à convertir -->
                     <div class="col">
                         <label for="litre" class="form-label visually-hidden">Montant à convertir :</label>
                         <div class="input-group">
                             <input id="litre" name="litre" type="number" class="form-control" required>
+                            <!-- Indicateur de l'unité de mesure -->
                             <div class="input-group-append">
                                 <div class="input-group-text">L</div>
                             </div>
                         </div>
                     </div>
 
-                    <!-- Texte indiquant la devise cible -->
+                    <!-- Indicateur de conversion -->
                     <div class="col-auto align-self-center">
                         <span class="ver">vaut :</span>
                     </div>
 
-                    <!-- Champ pour afficher le résultat de la conversion en millilitres -->
+                    <!-- Champ pour afficher le résultat de la conversion -->
                     <div class="col">
                         <label for="mil" class="form-label visually-hidden">Résultat</label>
                         <div class="input-group">
                             <input id="mil" name="mil" type="text" class="form-control" disabled>
+                            <!-- Indicateur de l'unité de mesure -->
                             <div class="input-group-append">
                                 <div class="input-group-text">mL</div>
                             </div>
                         </div>
                     </div>
 
+                    <!-- Bouton pour soumettre le formulaire -->
                     <div class="col-auto">
                         <button name="submit" type="submit" class="btn btn-primary">Calculer</button>
                     </div>
@@ -153,20 +169,18 @@
             </form>
         </fieldset>
     </div>
-</section>
+</section><!-- End Liquid Converter Section -->
 
-<!-- Script JavaScript pour gérer la soumission du formulaire via AJAX -->
+<!-- Script JavaScript pour la gestion des soumissions de formulaire -->
 <script type="text/javascript">
     window.addEventListener('load', () => {
         let forms = document.forms;
         for (form of forms) {
             form.addEventListener('submit', async (event) => {
                 event.preventDefault();
-                
-                // Récupération des données du formulaire
+
                 const formData = new FormData(event.target).entries();
 
-                // Envoi des données du formulaire à l'API via une requête POST
                 const response = await fetch('/api/post', {
                     method: 'POST',
                     headers: {
@@ -179,11 +193,11 @@
                     )
                 });
 
-                // Récupération des résultats de la conversion depuis la réponse de l'API
                 const result = await response.json();
-                
-                // Mise à jour des champs de formulaire avec les résultats de la conversion
+
                 let inputName = Object.keys(result.data)[0];
+
+                // Mettre à jour le champ de formulaire correct en fonction du nom du formulaire
                 if (event.target.name === 'euros-dollars') {
                     event.target.querySelector(`input[name="devisecible"]`).value = result.data[inputName];
                 } else if (event.target.name === 'mL-L') {
@@ -194,4 +208,7 @@
     });
 </script>
 
-<?php template('footer');
+<?php
+
+template('footer');
+?>
