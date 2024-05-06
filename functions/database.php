ec7b86b8deb6ae43caa86d921e88912c8950222f
Frederic Vinet <fvinet9@gmail.com>
2024-05-06 10:18:44 +0200

    Vendor fonctionnel

--- functions/database.php
+++ functions/database.php
@@ -7,7 +7,7 @@
  * @return mixed 
  */
 function run_query(string $query) {
-    $connection  = @mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT);
+    $connection  = @mysqli_connect($_ENV['DB_HOST'], $_ENV['DB_USER'], $_ENV['DB_PASSWORD'], $_ENV['DB_NAME'], $_ENV['DB_PORT']);
     
     if (mysqli_connect_errno()) {
         // Lance une exception avec un message d'erreur
