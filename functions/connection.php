f38ce213fbf8027f88001963fa3d5e34eeafee1a
Frederic Vinet <fvinet9@gmail.com>
2024-05-06 10:44:55 +0200

    Commentaires

--- functions/connection.php
+++ functions/connection.php
@@ -1,10 +1,15 @@
 <?php
 
+// Inclut le fichier autoload.php généré par Composer.
 require_once __DIR__ . '/../vendor/autoload.php';
 
+// Importe la classe Dotenv de la bibliothèque Dotenv.
 use Dotenv\Dotenv;
 
+// Crée une instance de Dotenv avec le répertoire racine du projet.
 $dotenv = Dotenv::createImmutable(__DIR__);
+
+// Charge les variables d'environnement à partir du fichier .env dans le répertoire racine du projet.
 $dotenv->load();
 
 // $db = include ROOT_PATH . 'config.php';
